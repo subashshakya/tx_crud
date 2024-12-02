8491b09398ff566908f94064ef4b5e43cce776bd
subashshakya <subashshakya12@gmail.com>
2024-12-02 08:20:00 +0545

    [FEAT] update routes and initial page routing strategies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: 'transactions',
+    loadChildren: () =>
+      import('./transaction/transaction.module').then(
+        (m) => m.TransactionModule
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'user',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import(
+        './shared/components/route-not-found/route-not-found.component'
+      ).then((c) => c.RouteNotFoundComponent),
+  },
+];
